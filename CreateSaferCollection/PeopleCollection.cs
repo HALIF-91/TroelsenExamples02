d6ccd7e41f64bb8b3b6a25588e2801542fb31d32
ADIL <globalmail91@gmail.com>
2019-12-02 14:26:06 +0600

    проблемы с безопасностью типов

---
+++ CreateSaferCollection/PeopleCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateSaferCollection
+{
+    public class PeopleCollection : IEnumerable
+    {
+        private ArrayList arPeople = new ArrayList();
+        public Person GetPerson(int pos)
+        {
+            return (Person)arPeople[pos];
+        }
+        public void AddPerson(Person p)
+        {
+            arPeople.Add(p);
+        }
+        public void ClearPeople()
+        {
+            arPeople.Clear();
+        }
+        public int Count()
+        {
+            return arPeople.Count;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return arPeople.GetEnumerator();
+        }
+    }
+}
---
+++ CreateSaferCollection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateSaferCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PeopleCollection people = new PeopleCollection();
+            people.AddPerson(new Person("Homer", "Simpson", 40));
+            people.AddPerson(new Person("Marge", "Simpson", 38));
+            people.AddPerson(new Person("Lisa", "Simpson", 9));
+            people.AddPerson(new Person("Bart", "Simpson", 7));
+            people.AddPerson(new Person("Maggie", "Simpson", 2));
+
+            // Это вызовет ошибку при компиляции!
+            // people.AddPerson(new Car());
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+            Console.ReadLine();
+        }
+    }
+}
