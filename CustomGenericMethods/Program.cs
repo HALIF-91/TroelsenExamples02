dd3d24b588d8a0ea55d6f1781aeef54335dc0f33
ADIL <globalmail91@gmail.com>
2019-12-05 00:08:10 +0600

    создание специальных обобщенных методов

---
+++ CustomGenericMethods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomGenericMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // обмен двух значений int
+            int a = 10, b = 90;
+            Console.WriteLine("Before swap: {0}, {1}", a, b);
+            Swap(ref a, ref b);
+            Console.WriteLine("After swap: {0}, {1}", a, b);
+            Console.WriteLine();
+
+            // обмен двух строк
+            string s1 = "Hello", s2 = "There";
+            Console.WriteLine("Before swap: {0}, {1}", s1, s2);
+            Swap(ref s1, ref s2);
+            Console.WriteLine("After swap: {0}, {1}", s1, s2);
+            Console.WriteLine();
+
+            // Необходимо указать параметр типа,
+            // если метод не принимает параметров
+            DisplayBaseClass<int>();
+            DisplayBaseClass<string>();
+
+            // Ошибка на этапе компиляции
+            // DisplayBaseClass();
+            Console.ReadLine();
+        }
+        static void Swap<T>(ref T a, ref T b)
+        {
+            Console.WriteLine("You sent the Swap() method a {0}", typeof(T));
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        static void DisplayBaseClass<T>()
+        {
+            // BaseType - это метод используемый в рефлексии;
+            Console.WriteLine("Base class of {0} is: {1}.", typeof(T), typeof(T).BaseType);
+        }
+    }
+}
---
+++ GenericPoint/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPoint
+{
+    public struct Point<T>
+    {
+        private T xPos;
+        private T yPos;
+        public Point(T xVal, T yVal)
+        {
+            xPos = xVal;
+            yPos = yVal;
+        }
+        public T X
+        {
+            get { return xPos; }
+            set { xPos = value; }
+        }
+        public T Y
+        {
+            get { return yPos; }
+            set { yPos = value; }
+        }
+        public override string ToString()
+        {
+            return String.Format("[{0}, {1}]", xPos, yPos);
+        }
+        // Сбросить поля до стандартного значения
+        // для заданного параметра типа T
+        public void ResetPoint()
+        {
+            xPos = default(T);
+            yPos = default(T);
+        }
+    }
+}
