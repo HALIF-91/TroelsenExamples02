01d8e1443bfa0f81d553770eeedcd53a8d3c7a18
ADIL <globalmail91@gmail.com>
2019-12-08 01:11:38 +0600

    прослушивание входящих событий

---
+++ CarEvents/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CarEvents
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+        private bool carIsDead;
+
+        public Car() { MaxSpeed = 100; }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        public delegate void CarEngineHandler(string msg);
+
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler AboutToBlow;
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                if (Exploded != null)
+                    Exploded("Sorry, this car is dead");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+
+                if ((MaxSpeed - CurrentSpeed) <= 10 && AboutToBlow != null)
+                    AboutToBlow("Careful buddy! Gonna blow!");
+                
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
---
+++ CarEvents/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CarEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            // Регистрация обработчиков событий
+            c1.AboutToBlow += CarIsAlmostDoomed;
+            c1.AboutToBlow += CarAboutToBlow;
+            c1.Exploded += CarExploded;
+
+            Console.WriteLine("***** Speeding up ******");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            c1.Exploded -= CarExploded;
+
+            Console.WriteLine("***** Speeding up ******");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            Console.ReadLine();
+        }
+
+        private static void CarExploded(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        private static void CarAboutToBlow(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        private static void CarIsAlmostDoomed(string msg)
+        {
+            Console.WriteLine("=> Critical Message from Car: {0}", msg);
+        }
+    }
+}
