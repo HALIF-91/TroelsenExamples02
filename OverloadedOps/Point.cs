6e5826b927aba514d62fd7e3029157d834f0403d
ADIL <globalmail91@gmail.com>
2019-12-16 19:45:22 +0600

    перегрузка бинарных операций

---
+++ OverloadedOps/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int xPos, int yPos)
+        {
+            X = xPos; Y = yPos;
+        }
+        public Point() { }
+        public override string ToString()
+        {
+            return string.Format("[{0}, {1}]", X, Y);
+        }
+
+        #region Перегруженные операции + и -
+        public static Point operator + (Point p1, Point p2)
+        {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y);
+        }
+        public static Point operator - (Point p1, Point p2)
+        {
+            return new Point(p1.X - p2.X, p1.Y - p2.Y);
+        }
+        #endregion
+
+        #region Перегруженная операция + с числовым смещением
+        public static Point operator + (Point p, int change)
+        {
+            return new Point(p.X + change, p.Y + change);
+        }
+        // При смене слагаемых
+        public static Point operator + (int change, Point p)
+        {
+            return new Point(p.X + change, p.Y + change);
+        }
+        #endregion
+    }
+}
---
+++ OverloadedOps/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point ptOne = new Point(100, 100);
+            Point ptTwo = new Point(40, 40);
+
+            Console.WriteLine("ptOne = {0}", ptOne);
+            Console.WriteLine("ptTwo = {0}", ptTwo);
+
+            // Сложить две точки чтобы получить большую
+            Console.WriteLine("ptOne + ptTwo: {0}", ptOne + ptTwo);
+
+            // Вычесть одну точку из другой чтобы получить меньшую
+            Console.WriteLine("ptOne - ptTwo: {0}", ptOne - ptTwo);
+
+            // Выводит [110, 110]
+            Point biggerPoint = ptOne + 10;
+            Console.WriteLine("ptOne + 10 = {0}", biggerPoint);
+
+            // Смена слагаемых, выводит [110, 110]
+            Point biggerPoint2 = 10 + ptOne;
+            Console.WriteLine("10 + ptOne = {0}", biggerPoint2);
+            Console.ReadLine();
+        }
+    }
+}
